82054204888184467c8ff19543f78ae03f597670
Nikita <nikitavolkov.volk@mail.ru>
2024-06-01 01:27:47 +0500

    change

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{b as a}from"./chunk-DVVS37ZH.mjs";import"./chunk-BTSFRDPS.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{b as a}from"./chunk-RS4XBB3J.mjs";import"./chunk-BTSFRDPS.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
